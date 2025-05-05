d93c7651c410040523fa7269989f24cea62a4455
Faith <kashfaith77@gmail.com>
2025-05-05 15:34:01 +0300

    python functions

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,28 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+
+greet_programmer()    
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+
+greet("faith")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+greet_with_default()    
 
 def add(num1, num2):
-    pass
+    return num1 +num2
+print(add(1,2))
+
 
 def halve(number):
-    pass
+    if not (number, (int, float)):
+        return None
+    return number/2
+print(halve(10))
+
+
